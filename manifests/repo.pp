26cf502822ef447a658908034cc6eec8635e860b
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-01-13 08:14:45 -0500

    Add solr::repo to setup repo if needed

---
+++ manifests/repo.pp
@@ -0,0 +1,43 @@
+# == Class solr::repo
+# Configures the Apache Bigtop archive repoisitory.
+#
+# === Parameters
+#
+# [*release*]
+#   The Apache Bigtop release to deploy. Defaults to 0.7.0 current at this time.
+#
+# === Examples
+#
+#  class { 'solr::repo':
+#    release = '0.5.0'
+#  }
+#
+# === Authors
+#
+# Jeremy T. Bouse <jeremy.bouse@undergrid.net>
+#
+# === Copyright
+#
+# Copyright 2014 UnderGrid Network Services
+#
+class solr::repo (
+  $release = '0.7.0'
+) {
+  $baseurl = 'http://bigtop.s3.amazonaws.com/releases'
+  $gpgkeyurl = 'http://archive.apache.org/dist/bigtop/KEYS'
+
+  if $::osfamily == 'RedHat' {
+    $dist = $::operatingsystem ? {
+      default => $::lsbmajdistrelease,
+      Amazon  => 6,
+    }
+
+    yumrepo { 'bigtop':
+      baseurl  => "${baseurl}/${release}/redhat/${dist}/${::architecture}",
+      enabled  => 1,
+      gpgcheck => 1,
+      gpgkey   => $gpgkeyurl,
+      descr    => "Apache Bigtop El ${dist} - ${::architecture}",
+    }
+  }
+}
